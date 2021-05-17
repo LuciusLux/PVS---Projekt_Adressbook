e5bd013cf55892a15e64a76d9b34641f84f1d3a2
LuciusLux <77528299+LuciusLux@users.noreply.github.com>
2021-05-17 23:57:48 +0200

    Search field and fields

--- app_contact/views.py
+++ app_contact/views.py
@@ -3,12 +3,19 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Contact, Address
 from .serializers import ContactSerializer, AddressSerializer
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
 
 # Create your views here.
 
 class ContactApiView(viewsets.ModelViewSet):
     serializer_class = ContactSerializer
     queryset = Contact.objects.all()
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filterset_fields = ['type']
+    search_fields = ['name', 'first_name']
+
+
 
 
 class AddressApiView(viewsets.ModelViewSet):
