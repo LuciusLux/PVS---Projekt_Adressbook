ce1cc1e59eaa57df0be872ebc81f9d338120f920
LuciusLux <77528299+LuciusLux@users.noreply.github.com>
2021-05-17 23:14:27 +0200

    Library installieren pipenv install drf-yasg
    Library registrieren
    in settings.py add
    INSTALLED_APPS = [
        ...
        'drf_yasg',
        ...
    
    Url definieren
    in urls.py
    from rest_framework import permissions
    from drf_yasg.views import get_schema_view
    from drf_yasg import openapi
    
    ...
    
    schema_view = get_schema_view(
       openapi.Info(
          title="Contact DB API",
          default_version='v1',
          description="School project",
          contact=openapi.Contact(email="contact@snippets.local"),
          license=openapi.License(name="BSD License"),
       ),
       public=True,
       permission_classes=(permissions.AllowAny,),
    )
    
    ...
    
    urlpatterns = [
    ...
        path('api/v1/doc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc-ui'),
    ...

--- app_contact/urls.py
+++ app_contact/urls.py
@@ -1,6 +1,7 @@
 from rest_framework import routers
 from .views import ContactApiView, AddressApiView
 
+
 router = routers.DefaultRouter()
 router.register('contacts', ContactApiView)
 router.register('addresses', AddressApiView)
